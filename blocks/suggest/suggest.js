2298b6c3cd4081196c69f25317e495ca5a4ca70b
Vladimir Grinenko <i@tadatuta.com>
2016-11-25 03:12:03 +0300

    Fix long item click

--- blocks/suggest/suggest.js
+++ blocks/suggest/suggest.js
@@ -192,7 +192,6 @@ provide(BemDom.decl(this.name, {
 
     _onPopupPointerPress : function() {
         this._needRefocusControl = true;
-        this.unbindFrom(this._popup.domElem, 'pointerpress', this._onPopupPointerPress);
         this.bindToDoc('pointerrelease', this._refocusControl);
     },
 
